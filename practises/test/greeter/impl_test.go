2ef7f8174608bff618cfad99d60afbc8f86c9187
tursunbekzat <b_tursun@kbtu.kz>
2025-01-23 07:35:58 +0500

    adding TableTest and improving existing tests

--- practises/test/greeter/impl_test.go
+++ practises/test/greeter/impl_test.go
@@ -7,10 +7,41 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGreet(t *testing.T){
-	res := greeter.Greet("Bekzatt")
+// func TestGreet(t *testing.T){
+// 	res := greeter.Greet("Bekzat")
 
-	const expected  = "Hello Bekzat"
+// 	const expected  = "Hello Bekzat"
 
-	require.Equal(t, expected, res)
+// 	require.Equal(t, expected, res)
+// }
+
+func TestTableGreet(t *testing.T){
+	cases := []struct{
+		desc string
+		input string
+		expect string
+	}{
+		{
+			desc: "case1",
+			input: "",
+			expect: "Hello world",
+		},
+		{
+			desc: "case2",
+			input: "Bekzat",
+			expect: "Hello Bekzat",
+		},
+	}
+
+	for i := range cases{
+		tC := cases[i]
+
+		t.Run(tC.desc, func(t *testing.T) {
+			t.Parallel()
+
+			res := greeter.Greet(tC.input)
+	
+			require.Equal(t, tC.expect, res)
+		})
+	}
 }
--- practises/test/tasks/tesk1_test.go
+++ practises/test/tasks/tesk1_test.go
@@ -24,3 +24,44 @@ func TestTitleCaseWithMinorInFirst(t *testing.T){
 	got := tasks.TitleCase(str, minor)
 	require.Equal(t, want, got)
 }
+
+
+type Cases struct{
+	desc string
+	str string
+	minor string
+	want string
+}
+
+func TestTableTitleCaseAllCases(t *testing.T){
+	cases := []Cases{
+		{
+			desc: "case1",
+			str: "",
+			minor: "",
+			want: "",
+		},
+		{
+			desc: "case2",
+			str: "My name is Bekzat",
+			minor: "",
+			want: "MY NAME IS BEKZAT",
+		},
+		{
+			desc: "case3",
+			str: "My name is Bekzat",
+			minor: "my bekzat",
+			want: "MY NAME IS bekzat",
+		},		
+	}
+
+	for i := range cases{
+		tC := cases[i]
+
+		t.Run(tC.desc, func(t *testing.T) {
+			t.Parallel()
+			got := tasks.TitleCase(tC.str, tC.minor)
+			require.Equal(t, tC.want, got)
+		})
+	}
+}
